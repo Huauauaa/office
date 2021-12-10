ed572f466e508e9a9c8c3c672b2703d31ff4e86d
huaweiwei <harvey0379@163.com>
2021-12-10 10:14:02 +0800

    use sqllite

--- README.md
+++ README.md
@@ -4,19 +4,10 @@
 
 ## prepare
 
-- `python3 -m venv venv` `. venv/bin/activate`
+- `python3 -m venv venv`
+- `. venv/bin/activate`
 
 ### Database
-
-```sql
-create database <db_name>
-```
-
-```env
-DB_USERNAME=
-DB_PASSWORD=
-DB_DATABASE=
-```
 
 #### init db
 
@@ -35,11 +26,11 @@ DB_DATABASE=
 - `pip install -r requirements.txt`
 - `pnpm install`
 
-## Run
-
-- `flask run`
-- `pnpm run dev`
-
 ## Build
 
 - `pnpm run build`
+
+## Dev run
+
+- `flask run -p 8888`
+- `pnpm run dev`
--- config.py
+++ config.py
@@ -1,13 +1,7 @@
 import os
 
-
-HOSTNAME = os.environ.get('DB_HOSTNAME') or 'localhost'
-PORT = os.environ.get('DB_PORT') or '3306'
-DATABASE = os.environ.get('DB_DATABASE') or 'office'
-USERNAME = os.environ.get('DB_USERNAME')
-PASSWORD = os.environ.get('DB_PASSWORD')
-DB_URI = f'mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}?charset=utf8'
-SQLALCHEMY_DATABASE_URI = DB_URI
-
+SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
 SECRET_KEY = 'asdasdfasdfasdfasdfasd'
